1a3e0c93b53ac6f2a0c0c3de3b02719569450259
Saleh Shah <saleh@nuxera.ai>
2026-01-23 13:06:10 +0500

    Remove AudioCapture reference from index.tsx

--- test/index.tsx
+++ test/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TestApp from "./TestApp";
-import AudioCaptureExample from "./AudioCaptureExample";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
